10126c8979c4ba4c99388fda918ac8a4c70f22a3
Thales Bensi <thalesbensi@outlook.com>
2024-08-16 15:08:41 -0300

    OOP setup

--- src/entities/OutsourcedEmployee.java
+++ src/entities/OutsourcedEmployee.java
@@ -1,4 +1,31 @@
 package entities;
 
-public class OutsourcedEmployee {
+public class OutsourcedEmployee extends Employee {
+
+    private Double additionalCharge;
+
+    public OutsourcedEmployee(){
+
+    }
+
+    public OutsourcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+        super(name, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
+    @Override
+    public double payment(){
+        return hours * valuePerHour + ((additionalCharge * 110) / 100);
+    }
+
+
+
 }
